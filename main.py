f318339cc235e33f764d81fc2d7f9d8dc16ca66d
copsocmg2-dev <copsocmg2@gmail.com>
2026-02-26 16:04:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,9 @@ import pytz
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 # --- Importações do Google Sheets ---
 from google.oauth2 import service_account
